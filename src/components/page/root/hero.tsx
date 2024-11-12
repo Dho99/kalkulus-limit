79011bdedc0201e72a13b6bebac6101ced4bad76
Ridho A. <ridhoawwaludin@gmail.com>
2024-11-12 22:40:17 +0700

    Remove unused import

--- src/components/page/root/hero.tsx
+++ src/components/page/root/hero.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Box, Container, Flex, Text, Image, Button } from "@chakra-ui/react";
-import { ArrowForwardIcon } from "@chakra-ui/icons";
+// import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 export default function Hero() {
   return (
@@ -10,7 +10,7 @@ export default function Hero() {
         <Flex gap={8} alignItems={"center"} height={"40vh"}>
           <Box flexBasis="55%">
             <Text textStyle={"6xl"} fontWeight={"bolder"}>Buat belajar Limit anda menjadi lebih Mudah !</Text>
-            <Button mt={8} colorPalette={"yellow"} size={"xl"}><Text fontWeight={"bold"}>Lihat Materi <ArrowForwardIcon/></Text></Button>
+            <Button mt={8} colorPalette={"yellow"} size={"xl"}><Text fontWeight={"bold"}>Lihat Materi </Text></Button>
           </Box>
           <Box flexBasis="45%">
             <Image src={"/Images/math_ilustration.svg"} rounded={"full"}/>

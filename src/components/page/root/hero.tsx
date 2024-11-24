376762f416a5ee5a0b851fa9197f36e673182470
Ridho A. <ridhoawwaludin@gmail.com>
2024-11-24 19:39:12 +0700

    Update Lihat materi button

--- src/components/page/root/hero.tsx
+++ src/components/page/root/hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Box, Container, Flex, Text, Image, Button } from "@chakra-ui/react";
-import { FaArrowRight } from "react-icons/fa";
+import { FaArrowCircleDown } from "react-icons/fa";
 
 export default function Hero() {
   return (
@@ -20,12 +20,14 @@ export default function Hero() {
             Jelajahi dunia kalkulus melalui pembelajaran interaktif dan menarik. Mari mulai perjalanan matematika Anda. Klik tombol Dibawah !
             </Text>
             <Flex flexDir={"column"} gap={4} w={"fit-content"} mt={"60px"}>
+              <a href="#materi">
               <Button colorPalette={"blue"} size={"2xl"} rounded={"3xl"} _hover={{bg: "blue.400", shadow: "md"}} shadow={"sm"}>
                 <Text fontWeight={"bold"} display="flex" alignItems={"center"} me={"auto"} px={1}>
                   Lihat Materi 
-                  <Text ms={3} rotate={"90deg"}><FaArrowRight/></Text>
+                  <Text ms={3}><FaArrowCircleDown /></Text>
                 </Text>
               </Button>
+              </a>
             </Flex>
           </Box>
           <Box width={{xl: "50vw", lg: "50vw", md: "50vw", sm: "75vw", base: "75vw"}} order={{xl: "2", lg: "2", md: "2", sm: "1", base: "1"}}>
@@ -33,7 +35,7 @@ export default function Hero() {
               src={"/Images/math_ilustration.svg"}
               rounded={"full"}
               alt={"Math Illustration Image"}
-              shadow={"md"}
+              shadow={"2xl"}
             />
           </Box>
         </Box>
